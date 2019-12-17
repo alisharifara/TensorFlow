9a1a043dc089dc68bcc2f765578564442586cb28
Ali <a.sharifara@gmail.com>
2019-12-17 02:38:43 -0600

    Add files via upload

---
+++ numpyInTensorFlow.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+
+sess = tf.Session()
+
+zeroD = np.array(30, dtype=np.int32)
+print(sess.run(tf.rank(zeroD)))
+print(sess.run(tf.shape(zeroD)))
+
+oneD = np.array([1.3, 4.3, 5.4, 43.2], dtype=np.float32)
+print(sess.run(tf.rank(oneD)))
+print(sess.run(tf.shape(oneD)))
+
+sess.close()
