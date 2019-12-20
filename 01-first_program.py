d147d66722dda20cf84e9f313866041cc155254c
Ali <a.sharifara@gmail.com>
2019-12-19 20:30:48 -0600

    Update 01-first_program.py

--- 01-first_program.py
+++ 01-first_program.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 
-
 a = tf.constant(6.5, name='constant_a')
 b = tf.constant(3.4, name='constant_b')
 c = tf.constant(3.0, name='constant_c')
@@ -12,6 +11,7 @@ sqrt = tf.sqrt(d, name='sqrt_d')
 
 final_sum = tf.add_n([square, power, sqrt], name='final_sum')
 
+# start a new session
 sess = tf.Session()
 
 print('squre of a:', sess.run(square))
@@ -21,10 +21,10 @@ print('final sum:', sess.run(final_sum))
 
 another_sum = tf.add_n([a, b, c, d, power], name='another_sum')
 
-writer = tf.summary.FileWriter('./m2_example2', sess.graph)
-
+# to create an output to tensorboard
+writer = tf.summary.FileWriter('./output', sess.graph)
 writer.close()
 sess.close()
 
-# after that, I need to go to terminal and type the following:
-# tensorboard --logdir="m2_example2"
+# after that, you need to go to terminal and type the following:
+# tensorboard --logdir="output"
